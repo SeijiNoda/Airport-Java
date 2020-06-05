8efc6ff0d5e756328eb0e454f56efd570f6f5e7e
EnzoSpinella <platinamaster5@gmail.com>
2020-06-05 20:45:48 -0300

    Tried to begin the Principal form's development in IntelliJ

---
+++ src/Programa/Inicial.java
@@ -0,0 +1,16 @@
+package Programa;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Inicial extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
